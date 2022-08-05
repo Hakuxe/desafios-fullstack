32cae2c6398c3b4add40c71ff543bc8cee145cb8
Hakuxe <arthurnorton.jobs@gmail.com>
2022-08-05 20:03:24 -0300

    criando rota para cadastro de itens na tabela

--- webpage/server/src/server.ts
+++ webpage/server/src/server.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import cors from "cors";
 
 import database from "./database/data";
 
 const PORT = 3030;
 const app = express();
 
-// app.use(cors); 
 app.use(express.json());
 
 app.listen(PORT, () => console.log("server rodando..."));
@@ -16,3 +14,19 @@ app.get("/all", (request, response) => {
 	response.json(database);
 });
 
+app.post("/user", (request, response) =>{
+   const {user} = request.body;
+
+   const newItem = {
+      id: database.length + 1,
+		name: user.name,
+		birthDate: new Date(user.birthDate),
+		phone: user.phone,
+		email: user.email,
+   }
+
+   database.push(newItem);
+
+   response.send("Cadastro realizado com sucesso");
+
+})
